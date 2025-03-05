be53711347b94bf452661384042eed49b2a1b0d2
yassinejomaa <yassine.jomaa@gmail.com>
2025-03-05 15:01:13 +0100

    add sidebar collapse fonctionality

--- src/app/user/side-navbar/side-navbar.component.ts
+++ src/app/user/side-navbar/side-navbar.component.ts
@@ -12,5 +12,10 @@ import { Component } from '@angular/core';
     '../../../../public/css/teamplate/responsive.css']
 })
 export class SideNavbarComponent {
+  changeMenu=false;
+  menuChange(event: Event) {
+    event.stopPropagation(); // Empêche la propagation pour éviter de masquer immédiatement
+    this.changeMenu = !this.changeMenu;
   
+  }
 }
